0ed88010bdde2da7245686ea00b907ec11fce582
고병욱 <rhquddnr95@naver.com>
2023-05-30 20:44:00 +0900

    Feat : 레이아웃 / 메인페이지 / 마이페이지 라우팅 부분 구현 및 테스트

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,2 +1,7 @@
-const Footer = () => {};
+import React from "react";
+
+const Footer = () => {
+  return <footer>Footer</footer>;
+};
+
 export default Footer;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,2 +1,7 @@
-const Header = () => {};
+import React from "react";
+
+const Header = (): JSX.Element => {
+  return <header>Header Component</header>;
+};
+
 export default Header;
