a5195bab932ebd1acf6f7b6556802ea2868c4640
uniqueflowSA <saco2020a@gmail.com>
2023-05-31 21:49:50 +0900

    mypage-default-layout-buliding

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,7 +1,36 @@
+import React from 'react'
+import {
+  Grid
+} from '@mui/material';
+import MenuButton from './components/MenuButton'
+const Info = ()=>{
+  return (
+    <p>솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라
+      솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라
+      솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라
+      솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라
+      솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라
+      솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라
+
+    </p>
+  )
+}
+
 const Mypage = () => {
   return (
     <div>
       <p> 마이페이지 </p>
+      <Grid container>
+        <Grid item xs = {2}>
+          <MenuButton />
+        </Grid>
+        <Grid item xs ={9}>
+          <Info/>
+        </Grid>
+        <Grid item xs ={1}>
+        </Grid>
+      </Grid>
+      
     </div>
   );
 };
