87af54bf9d8daae41decc638fe9c4277307c7a64
frefloww <frefloww@gmail.com>
2023-05-31 19:10:25 +0900

    Chore : PORT, DB 연결

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,7 @@
 const mongoose = require('../config/database');
 
-const ProductSchema = require('./schemas/product');
-const CartSchema = require('./schemas/cart');
-const OrderSchema = require('./schemas/order');
+const CommunitySchema = require('./schemas/community');
 const UserSchema = require('./schemas/user');
 
-exports.Product = mongoose.model('Product', ProductSchema);
-exports.Cart = mongoose.model('Cart', CartSchema);
-exports.Order = mongoose.model('Order', OrderSchema);
+exports.Community = mongoose.model('Community', CommunitySchema);
 exports.User = mongoose.model('User', UserSchema);
