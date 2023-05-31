3c152655a2b5add26dd962f50222a1e523185525
merry <merrykang1103@gmail.com>
2023-05-31 19:22:37 +0900

    [Feat] 피드백 스키마에 content_type을 실질적으로 boolean으로 사용

--- src/models/schemas/study_feedback.js
+++ src/models/schemas/study_feedback.js
@@ -1,13 +1,13 @@
 const { Schema } = require('mongoose');
 
 const StudyFeedbackSchema = new Schema({
-  study_id: { type: Number, required: true, unique: true, ref: 'Study' }, // reference
+  study_id: { type: mongoose.Types.ObjectId, required: true, unique: true, ref: 'Study' }, // reference
   study_name: { type: String, required: true, ref: 'Study' }, // reference
-  user_id: { type: Number, required: true, unique: true, ref: 'User' }, // reference
-  name: { type: String, required: true, ref: 'User' }, // reference
-  date: { type: Date, required: true, default: Date.now },
-  feedback: { type: String, required: true },
-  // 댓글 스키마로 어떻게 표현?
+  user_id: { type: mongoose.Types.ObjectId, required: true, unique: true, ref: 'User' }, // reference
+  user_name: { type: String, required: true, ref: 'User' }, // reference
+  content_type: { type: String, defalut: 'feedback', required: true }, // feedback: 피드백 본문, reply: 피드백 댓글
+  content: { type: String, required: true },
+  date: { type: Date, required: true, defalut: Date.now }, // 댓글작성일시
 });
 
 module.exports = StudyFeedbackSchema;
--- src/models/schemas/study_relation.js
+++ src/models/schemas/study_relation.js
@@ -1,8 +1,8 @@
 const { Schema } = require('mongoose');
 
 const StudyRelationSchema = new Schema({
-  study_id: { type: Number, required: true, unique: true, ref: 'Study' }, // reference
-  user_id: { type: Number, required: true, unique: true, ref: 'User' }, // reference
+  study_id: { type: mongoose.Types.ObjectId, required: true, unique: true, ref: 'Study' }, // reference
+  user_id: { type: mongoose.Types.ObjectId, required: true, unique: true, ref: 'User' }, // reference
   user_type: { type: String, defalut: 'member', required: true }, // leader: 스터디장, member: 스터디원
 });
 
