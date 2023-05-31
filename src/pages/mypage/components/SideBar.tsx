fa0672abd8a61f52ca2e144116a7e51fc92be78b
uniqueflowSA <saco2020a@gmail.com>
2023-05-31 21:49:50 +0900

    mypage-default-layout-buliding

---
+++ src/pages/mypage/components/SideBar.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {
+  Button,
+  Box,
+  Grid
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+
+const DeleteButton = styled(Button)({
+  color:'#d50000'
+})
+
+const MenuButton = ({}) =>{
+  return (
+    <Grid container>
+      <Grid item xs= {12}>
+      <Button variant="text">내 정보</Button>
+      </Grid >
+      <Grid item xs= {12}> 
+        <Button variant="text">개인정보수정</Button>
+        </Grid>
+      <Grid item xs= {12}>
+        <DeleteButton variant="text">회원탈퇴</DeleteButton>
+      </Grid>
+    
+   
+    
+  </Grid>
+  )
+}
+
+export default MenuButton
