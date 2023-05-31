051acb0b5df4ab4458ed39526bd08afab77f8f7b
SAEJIN PARK <cobaltcyan.park@gmail.com>
2023-06-01 01:34:44 +0900

    [Feat] user 스키마 추가

--- src/models/schemas/user.js
+++ src/models/schemas/user.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-
-        "user_id": {
-            type: Number
-        },
-        "name": {
-            type: String
-        }
-    }
-);
+    user_id: { type: mongoose.Types.ObjectId, required: true, unique: true }, // identification value
+    user_name: { type: String, required: true, unique: false },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: false },
+    privacy_use_yn: { type: String, required: true, unique: false },
+    marketing_use_yn: { type: String, required: true, unique: false },
+    intro_yn: { type: String, required: false, unique: false },               // 자기소개서
+    phone_number: { type: String, required: false, unique: false },
+    admin_yn: { type: boolean, required: false, unique: false },
+    dts_insert: {type: Date, required: true, unique: false }
+});
 
 module.exports = UserSchema;
