308f617002dd60a95f283e57318d9c93f77dec24
[SW4/2반] 공지아 <timing1018@gmail.com>
2023-06-01 20:00:38 +0900

    Feat : 회원가입 레이아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { RecoilRoot } from "recoil";
 import HomePage from "./pages/home/Homepage";
 import Mypage from "./pages/mypage/Mypage";
 import Layout from "./components/layout/Layout";
+import LoginPage from "./pages/login/Login";
+import SignupPage from "./pages/login/SignupPage";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +19,8 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/mypage" element={<Mypage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/login/signup" element={<SignupPage />} />
             </Routes>
           </Layout>
         </QueryClientProvider>
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,8 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <p>로그인페이지</p>
+    </div>
+  );
+};
+export default LoginPage;
