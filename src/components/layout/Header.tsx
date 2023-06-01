59c53c3ccaa4bb3d46dd60a776033b3d2714cfe9
Lee yong sub <dydtjq324@naver.com>
2023-06-01 14:29:16 +0900

    네브 대략적UI

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,72 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { Link } from "@mui/material";
 
+const Divider = styled.div`
+  margin: 15px 0px;
+  border-bottom: 1px solid #000;
+`;
+const StyledContainer = styled.div`
+  margin: 15px 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledLogo = styled(Link)`
+  font-size: 30px;
+  font-weight: bold;
+  color: #00057d;
+`;
+const StyledNavItemContainer = styled.div``;
+
+const StyledNavItem = styled(Link)`
+  font-size: 20px;
+  font-weight: bold;
+  color: black;
+  margin-left: 20px;
+`;
+const StyledLoginItemContainer = styled.div``;
+
+const StyledLoginItem = styled(Link)`
+  font-size: 15px;
+  font-weight: bold;
+  color: grey;
+  margin-left: 10px;
+`;
 const Header = (): JSX.Element => {
-  return <header>Header Component</header>;
+  return (
+    <>
+      <StyledContainer>
+        <StyledLogo href="#" underline="none">
+          면접왕
+        </StyledLogo>
+        <StyledNavItemContainer>
+          <StyledNavItem href="#" underline="none">
+            스터디
+          </StyledNavItem>
+          <StyledNavItem href="#" underline="none">
+            커뮤니티
+          </StyledNavItem>
+          <StyledNavItem href="#" underline="none">
+            나의 스터디
+          </StyledNavItem>
+        </StyledNavItemContainer>
+        <StyledLoginItemContainer>
+          <StyledLoginItem href="#" underline="none">
+            로그인
+          </StyledLoginItem>
+          <StyledLoginItem href="#" underline="none">
+            회원가입
+          </StyledLoginItem>
+          <StyledLoginItem href="#" underline="none">
+            마이페이지
+          </StyledLoginItem>
+        </StyledLoginItemContainer>
+      </StyledContainer>
+      <Divider></Divider>
+    </>
+  );
 };
 
 export default Header;
