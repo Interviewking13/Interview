0e7dcaf26aabd1cef179e817e92cafb00194bbf0
uniqueflowSA <saco2020a@gmail.com>
2023-06-01 20:16:00 +0900

    mypage-layout

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import {
+  Typography,
   Grid
 } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+
+
+//각 페이지컴포넌트 호출
 import MenuButton from './components/MenuButton'
+import MyInfo from './components/MyInfo'
+import MyStudy from './components/MyStudy'
+import StudyAP from './components/StudyApplyPage'
+
 const Info = ()=>{
   return (
     <p>솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라솰라
@@ -18,20 +28,43 @@ const Info = ()=>{
 
 const Mypage = () => {
   return (
-    <div>
-      <p> 마이페이지 </p>
-      <Grid container>
-        <Grid item xs = {2}>
-          <MenuButton />
-        </Grid>
-        <Grid item xs ={9}>
-          <Info/>
-        </Grid>
-        <Grid item xs ={1}>
+    <div style={{ maxWidth: '1270px', margin: 'auto' }}>
+     <Grid container spacing={1}>
+
+      {/* 타이틀과 서브 타이틀 */}
+      <Grid item xs={12}>
+        <Grid container spacing ={1}>
+          <Grid item>
+            <StyledTitle variant="h5">마이페이지</StyledTitle>
+          </Grid>
+          <Grid item>
+            <StyledSubTitle variant="subtitle1">나의 정보를 확인하세요</StyledSubTitle>
+          </Grid>
         </Grid>
       </Grid>
-      
+
+      {/* 버튼1, 버튼2, 버튼3 */}
+      <Grid item xs={12}>
+          <MenuButton/>
+      </Grid>
+
+      {/* 페이지 내용 */}
+      <Grid item xs={12}>
+        <Info/>
+      </Grid>
+    </Grid>
     </div>
   );
 };
 export default Mypage;
+
+
+const StyledTitle = styled(Typography )`
+  /* 여기에 스타일을 작성하세요 */
+  color: #00057D 
+`;
+
+const StyledSubTitle = styled(Typography )`
+  /* 여기에 스타일을 작성하세요 */
+  color: #8689A3 
+`;
---
+++ src/pages/mypage/components/MenuButton.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Button,
+  Box,
+  Grid,
+  Container
+} from '@mui/material'; //MUI 요소 import
+import { Create,Person, Checklist } from '@mui/icons-material'; //MUI icon import
+import { styled } from '@mui/material/styles';
+
+
+const MenuButton = ({}) =>{
+  return (
+    <Container maxWidth="xl">
+      <Grid container spacing={1} justifyContent='flex-start'>
+        <Grid item>
+          <StyledButton  variant="text" sx={{ gap: '5px' }}><Create/>나의 스터디</StyledButton>
+        </Grid >
+        <Grid item > 
+          <StyledButton  variant="text" sx={{ gap: '5px' }}><Checklist/>스터디 신청</StyledButton>
+        </Grid>
+        <Grid item >
+          <StyledButton  variant="text" sx={{ gap: '5px' }}><Person/>내 정보</StyledButton>
+        </Grid>
+      </Grid>
+    </Container>
+  
+  )
+}
+
+const StyledButton = styled(Button)`
+  /* 여기에 스타일을 작성하세요 */
+  color: #9FEFD3; /* 적절한 색상으로 변경 */
+  &:hover {
+    color: #00E595; /* 호버 시 변경할 색상 */
+  }
+  &:active {
+    background-color: #0000ff; /* 선택 시 변경할 색상 */
+  }
+`;
+
+export default MenuButton
