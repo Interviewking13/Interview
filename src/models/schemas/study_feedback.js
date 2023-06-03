84b23023d7918e2311bcee166ba42476f70f871e
frefloww <frefloww@gmail.com>
2023-06-03 13:39:54 +0900

    Feat : Community API CRUD

---
+++ src/models/schemas/study_feedback.js
@@ -0,0 +1,14 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+const StudyFeedbackSchema = new Schema({
+  study_id: { type: mongoose.Types.ObjectId, required: true, unique: true, ref: 'Study' }, // reference
+  study_name: { type: String, required: true, ref: 'Study' }, // reference
+  user_id: { type: mongoose.Types.ObjectId, required: true, unique: true, ref: 'User' }, // reference
+  user_name: { type: String, required: true, ref: 'User' }, // reference
+  content_type: { type: String, defalut: 'feedback', required: true }, // feedback: 피드백 본문, reply: 피드백 댓글
+  content: { type: String, required: true },
+  date: { type: Date, required: true, defalut: Date.now }, // 댓글작성일시
+});
+
+module.exports = StudyFeedbackSchema;
