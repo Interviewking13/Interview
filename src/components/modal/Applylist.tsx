a7d445522fe9a16149410c920051bb2b4c1cb60a
고병욱 <rhquddnr9502@gmail.com>
2023-06-03 10:21:50 +0700

    Feat : 더미데이터, applylist에 props로 데이터 전달

--- src/components/modal/Applylist.tsx
+++ src/components/modal/Applylist.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-interface ListItemProps {
+type ListItemProps = {
   text1: string;
   text2: string;
   imageSrc: string;
-}
+};
 
 const ListItem: React.FC<ListItemProps> = ({ text1, text2, imageSrc }) => {
   return (
@@ -16,27 +16,42 @@ const ListItem: React.FC<ListItemProps> = ({ text1, text2, imageSrc }) => {
     </StyledListItem>
   );
 };
-const ApplyList: React.FC = () => {
+
+type ApplyListProps = {
+  period: string;
+  deadline: string;
+  currentCount: number;
+  headCount: number;
+  studyLeader: string;
+};
+
+const ApplyList: React.FC<ApplyListProps> = ({
+  period,
+  deadline,
+  currentCount,
+  headCount,
+  studyLeader,
+}) => {
   return (
     <ListContainer>
       <ListItem
         text1="진행기간"
-        text2="23.05.02 ~ 23.08.01"
+        text2={period}
         imageSrc="./study-icon-img.png"
       />
       <ListItem
         text1="모집마감"
-        text2="23.05.01"
+        text2={deadline}
         imageSrc="./study-icon-img.png"
       />
       <ListItem
         text1="모집인원"
-        text2="3/10명"
+        text2={`${currentCount}/${headCount}명`}
         imageSrc="./study-icon-img.png"
       />
       <ListItem
         text1="스터디장"
-        text2="정채진"
+        text2={studyLeader}
         imageSrc="./study-icon-img.png"
       />
     </ListContainer>
@@ -52,12 +67,12 @@ const StyledListItem = styled.div`
 `;
 
 const StyledSpan = styled.span`
-  font-size: 18px;
+  font-size: 14px;
   margin-right: 70px; /* 원하는 마진 값 설정 */
 `;
 
 const StyledSpanContent = styled.span`
-  font-size: 18px;
+  font-size: 12px;
 `;
 
 const StyledImg = styled.img`
@@ -69,4 +84,5 @@ const StyledImg = styled.img`
 const ListContainer = styled.span`
   width: 923px;
   border-bottom: 1px solid #dadada;
+  margin-bottom: 20px;
 `;
