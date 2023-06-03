e667e9862d9b8991fe2ebed27a4404f9171fd490
고병욱 <rhquddnr95@naver.com>
2023-06-03 10:21:50 +0700

    Feat : 더미데이터, applylist에 props로 데이터 전달

--- src/components/modal/NestedModal.tsx
+++ src/components/modal/NestedModal.tsx
@@ -5,12 +5,37 @@ import Button from "@mui/material/Button";
 import styled from "styled-components";
 import ApplyList from "./Applylist";
 import { Link } from "react-router-dom";
+import { dateFomatting } from "../../utils/dateFomatting";
 
 export default function NestedModal() {
   const studyData = {
     title: "SAFFY 면접 스터디",
-    period: "2023-05-30~2023-06-08",
+    period: "2023-05-30 ~ 2023-06-08",
+    deadline: "2023-06-09",
+    headcount: 4,
+    study_id: 1,
+    study_name: "interview king",
+    content: "우리 스터디는 ~~을 목표로 하고, ...을 규칙으로 함",
+    chat_link:
+      "https://us05web.zoom.us/j/83754399005?pwd=QWRMY0I4VjhkWkhtdHdydkhTM0dLUT09",
+    status: 1,
+    currentCount: 2,
+    studyLeader: "채진짱",
   };
+
+  const {
+    title,
+    period,
+    deadline,
+    headcount,
+    currentCount,
+    study_id,
+    studyLeader,
+    study_name,
+    content,
+    chat_link,
+    status,
+  } = studyData;
 
   //취소버튼 이미지 링크
   const imageSrc = "./cancel-button.png";
@@ -25,20 +50,21 @@ export default function NestedModal() {
   return (
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="parent-modal-title"
-        aria-describedby="parent-modal-description"
-      >
+      <Modal open={open} onClose={handleClose}>
         <StyledBox>
           <StyledContainer>
             <StyledTopContainer>
               <StyledH2>스터디 신청하기</StyledH2>
               <StyledCancleImg src={imageSrc} alt="Cancel Button" />
             </StyledTopContainer>
-            <StyledTitle>SAFFY 면접 스터디</StyledTitle>
-            <ApplyList />
+            <StyledTitle>{title}</StyledTitle>
+            <ApplyList
+              period={dateFomatting(period)}
+              deadline={dateFomatting(deadline)}
+              currentCount={currentCount}
+              headCount={headcount}
+              studyLeader={studyLeader}
+            />
             <TextInput placeholder="한 줄 소개를 입력하시오. (60자 이내)" />
             <StyledBottom>
               <BottomContainer>
@@ -94,32 +120,32 @@ const StyledContainer = styled.div`
 const StyledH2 = styled.h2`
   font-size: 24px;
   margin: 0px;
-  margin-bottom: 30px;
+  margin-bottom: 22px;
   color: #00e595;
 `;
 
 const StyledTitle = styled.h2`
   font-size: 24px;
   margin: 0px;
-  margin-bottom: 30px;
+  margin-bottom: 22px;
   color: #00057d;
 `;
 
-const TextInput = styled.input`
+const TextInput = styled.textarea`
   width: 923px;
   height: 172px;
   border-radius: 10px;
-  text-align: center;
+  font-size: 18px;
 `;
 
 const StyledP = styled.p`
-  font-size: 12px;
+  font-size: 14px;
   margin: 0;
   margin-right: 10px;
 `;
 
 const StyledA = styled(Link)`
-  font-size: 12px;
+  font-size: 14px;
   text-decoration: none;
   color: #000;
   text-decoration: underline;
@@ -131,6 +157,8 @@ const StyledApplyButton = styled.button`
   border-radius: 10px;
   width: 132px;
   height: 45px;
+  font-size: 16px;
+  border: 0px;
 `;
 
 const StyledCancleImg = styled.img`
