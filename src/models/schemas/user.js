5760a175d1809e85a3ce95a2907dc4619d1d23a2
SAEJIN PARK <cobaltcyan.park@gmail.com>
2023-06-04 05:09:41 +0900

    [Feat] 1. user API 내 정보조회 추가 / 2. (Schema)user_id 타입 수정

--- src/models/schemas/user.js
+++ src/models/schemas/user.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    user_id: { type: mongoose.Types.ObjectId, required: true, unique: true }, // identification value
+    // user_id: { type: mongoose.Types.ObjectId, required: true, unique: true }, // identification value
+    user_id: { type: Number, required: true, unique: true }, // identification value
     user_name: { type: String, required: true, unique: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, unique: false },
