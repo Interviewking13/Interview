b4a44ae0788eeacd86ef928a304f8b7278c1591f
SAEJIN PARK <cobaltcyan.park@gmail.com>
2023-06-05 02:17:09 +0900

    [Feat] user API - 내 정보조회 및 수정 기능 추가

--- src/apis/user.js
+++ src/apis/user.js
@@ -16,7 +16,11 @@ const userApi = {
             res.send('userApi GET 방식 테스트');
             console.log('userApi 테스트1');        
         } catch (err) {
-            throw new Error(err);
+            console.error(err);
+            res.status(500).json({
+                resultCode: "500",
+                message: "서버오류"
+            })
         }
     },
 
@@ -31,7 +35,11 @@ const userApi = {
             })
             
         } catch (err) {
-            throw new Error(err);
+            console.error(err);
+            res.status(500).json({
+                resultCode: "500",
+                message: "서버오류"
+            })
         }
     },
 
@@ -86,8 +94,11 @@ const userApi = {
                 message: "회원가입 성공"
             })
         } catch (err) {
-            console.log(err);
-            throw new Error(err);
+            console.error(err);
+            res.status(500).json({
+                resultCode: "500",
+                message: "서버오류"
+            })
         }
     },
 
@@ -144,23 +155,95 @@ const userApi = {
     /** 내 정보 수정 */
     async modifyUserInfo(req, res, next) {
         try {
-            const { user_id, email, password, intro_yn, phone_number } = req.body;
-            const findUser = await User.findOne({ user_id });
-            const findInfo = User.update({ "email": email },
-                                {
-                                    $set: {
-                                        "email": email, 
-                                        "password": password,
-                                        "intro_yn": intro_yn,		//근데 기존에 값이 있고 그냥 다른 값만 변경하는 거라면 바꿔주기?
-                                        "phone_number": phone_number
-                                    }
-                                })
-            if (!findInfo) {
-                return res.status(400).json( { message: "내 정보 수정 실패" });
+            // const { user_id, email, password, intro_yn, phone_number } = req.body;
+            const { email, password, intro_yn, phone_number } = req.body;
+
+            // const findUser = await User.findOne({ "_id": user_id });    //나중에 user_id 값 사용가능하면? 사용가능할듯.
+            const findUser = await User.findOne({ "email": email });
+
+            if (!findUser) {
+                return res.status(400).json({
+                    resultCode: "400",
+                    message: "해당 사용자를 찾을 수 없습니다."
+                })
             }
-            res.send('내 정보 수정');
+            
+            // 기존 사용자 정보
+            const findUserId = findUser._id;
+            const findUserEmail = findUser.email;
+            const findUserPassword = findUser.password;
+            const findUserIntro_yn = findUser.intro_yn;
+            const findUserPhoneNumber = findUser.phone_number;
+
+            console.log(findUser);
+
+            // 변경사항 있는지 확인
+            let isModified = false;
+
+            if (email !== findUserEmail) {
+                findUser.email = email;
+                isModified = true;
+            }
+            if (password !== findUserPassword) {
+                findUser.password = password;
+                isModified = true;
+            }
+            if (intro_yn !== findUserIntro_yn) {
+                findUser.intro_yn = intro_yn;
+                isModified = true;
+            }
+            if (phone_number !== findUserPhoneNumber) {
+                findUser.phone_number = phone_number;
+                isModified = true;
+            }
+
+            // 변경사항이 없는 경우의 처리 로직
+            if(!isModified) {
+                return res.status(200).json({
+                    status: "200", 
+                    message: "변경사항이 없습니다."
+                });
+            }   
+            
+            // 변경사항이 있을 경우 처리 로직
+            const changeUserInfo = await User.updateOne({ "email": email },
+            {
+                $set: {
+                    "password": password,
+                    "intro_yn": intro_yn,
+                    "phone_number": phone_number
+                }
+            });
+
+            const changeFindUser = await User.findOne(
+                { "_id": findUser._id },
+                {
+                    "_id": true,
+                    "user_name": true,
+                    "email": true,
+                    "intro_yn": true,
+                    "phone_number": true
+                });
+
+            return res.status(200).json({
+                status: "200", 
+                message: "내 정보 수정 성공",
+                // data: changeUserInfo    // 안나와서 아래와 같이 수정
+                data: {
+                    user_id: findUser._id,
+                    email: findUser._id,
+                    intro_yn: findUser.intro_yn,
+                    phone_number: findUser.phone_number, 
+                }
+                
+            });
+
         } catch (err) {
-            throw new Error(err);
+            console.error(err);
+            res.status(500).json({
+                resultCode: "500",
+                message: "서버오류"
+            })
         }
     },
 
@@ -168,10 +251,38 @@ const userApi = {
     /** 회원탈퇴 */
     async deleteUser(req, res, next) {
         try {
+            const { user_id } = req.body;
 
-            res.send('회원탈퇴');
+            // TO-DO
+            const findUser = await User.findOne(
+                { "_id": user_id },
+                {
+                    "_id": true,
+                    "user_name": true,
+                    "email": true,
+                    "intro_yn": true,
+                    "phone_number": true
+                }
+            )
+            
+            if(!findUser) {
+                return res.status(400).json({
+                    resultCode: "400",
+                    message: "해당 사용자를 찾을 수 없습니다."
+                })
+            }
+            
+            // res.send('회원탈퇴 성공');
+            res.status(200).json({
+                resultCode: "200",
+                message: "회원탈퇴 성공"
+            })
         } catch (err) {
-            throw new Error(err);
+            console.error(err);
+            res.status(500).json({
+                resultCode: "500",
+                message: "서버오류"
+            })
         }
     }
 
