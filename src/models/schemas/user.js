d3f954ce3d5a30439ffcbcdc66d9d351a93dd917
merry <merrykang1103@gmail.com>
2023-06-04 20:44:53 +0900

    [Feat] 스터디장 권한 판단하는 study_relation API 추가

--- src/models/schemas/user.js
+++ src/models/schemas/user.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    user_id: { type: mongoose.Types.ObjectId, required: true, unique: true }, // identification value
-    user_name: { type: String, required: true, unique: false },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, unique: false },
-    privacy_use_yn: { type: String, required: true, unique: false },
-    marketing_use_yn: { type: String, required: true, unique: false },
-    intro_yn: { type: String, required: false, unique: false },
-    phone_number: { type: String, required: false, unique: false },
-    admin_yn: { type: Boolean, required: false, unique: false },
-    dts_insert: {type: Date, required: true, unique: false }
+  user_id: { type: mongoose.Types.ObjectId, unique: true }, // identification value
+  user_name: { type: String, unique: false },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, unique: false },
+  privacy_use_yn: { type: String, required: true, unique: false },
+  marketing_use_yn: { type: String, required: true, unique: false },
+  intro_yn: { type: String, required: false, unique: false },
+  phone_number: { type: String, required: false, unique: false },
+  admin_yn: { type: Boolean, required: false, unique: false },
+  dts_insert: { type: Date, required: true, unique: false },
 });
 
 module.exports = UserSchema;
