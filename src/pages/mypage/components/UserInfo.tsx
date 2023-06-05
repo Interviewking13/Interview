f2b27d02a4ad0e722681cd5a80511a47e4c5527f
uniqueflowSA <saco2020a@gmail.com>
2023-06-05 15:40:24 +0900

    내정보페이지구성중

--- src/pages/mypage/components/UserInfo.tsx
+++ src/pages/mypage/components/UserInfo.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import {
   Typography,
   Grid,
-  Container
+  Container,
+  Box,
+  Divider,
+  IconButton
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 //각 페이지컴포넌트 호출
 
@@ -12,10 +16,57 @@ import { styled } from '@mui/material/styles';
 
 const UserInfo = ()=>{
   return (
-    <div>
-      유저인포페이지 입니다.솰ㄹ솨로러ㅏㄴ와ㅣ
-    </div>
+    <StyledDiv>
+       <StyledTitle container xs={12}>
+            <Grid item xs={11}><Typography> 면접왕 준영님, 어서오세요. </Typography></Grid>
+            <Grid item><IconButton><SettingsIcon/></IconButton></Grid>
+        </StyledTitle>
+      <StyledLine></StyledLine>
+      <StyledInfo container>
+        <Grid item xs={12}>
+          <Grid container>
+            <Grid item xs={1}>아이디</Grid>
+            <Grid item>dasdsㅁㄴㅇㅁㄴ</Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container>
+            <Grid item xs={1}>비밀번호</Grid>
+            <Grid item>dasdsㅁㄴㅇㅁㄴ</Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container>
+            <Grid item xs={1}>연락처</Grid>
+            <Grid item>dasdsㅁㄴㅇㅁㄴ</Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container>
+            <Grid item xs={1}>자기소개서</Grid>
+            <Grid item>dasdsㅁㄴㅇㅁㄴ</Grid>
+          </Grid>
+        </Grid>
+      </StyledInfo>
+    </StyledDiv>
   )
 }
 
 export default UserInfo
+
+const StyledDiv = styled(Box)`
+  width: 1270px;
+  border: 1px solid #00057D;
+  border-radius: 7px;
+  `
+  const StyledTitle = styled(Grid)`
+    padding: 24px;
+    align-items: center
+  `
+  const StyledInfo = styled(Grid)`
+    margin: 24px;
+    font-size: 12pt;
+  `
+const StyledLine = styled(Divider)`
+  border: 1px solid #00057D;
+ `
