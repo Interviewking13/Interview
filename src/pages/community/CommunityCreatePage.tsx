b628df491b24f61fbca653098f297fa61aa0a29d
[SW4/2반] 공지아 <timing1018@gmail.com>
2023-06-07 01:02:49 +0900

    Feat: 커뮤니티 글쓰기 레이아웃 구현

--- src/pages/community/CommunityCreatePage.tsx
+++ src/pages/community/CommunityCreatePage.tsx
@@ -4,20 +4,19 @@ import styled from 'styled-components';
 const CommunityCreatePage: React.FC = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const [file, setFile] = useState<File | null>(null);
+  const [fileList, setFileList] = useState<File[]>([]);
 
-  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setTitle(e.target.value);
   };
 
-  const handleContentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleContentChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setContent(e.target.value);
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setFile(e.target.files[0]);
-    }
+    const selectedFiles = e.target.files ? Array.from(e.target.files) as File[] : [];
+    setFileList(selectedFiles);
   };
 
   const handleSubmit = () => {
@@ -34,27 +33,42 @@ const CommunityCreatePage: React.FC = () => {
 
   return (
     <StyledCreatePageContainer>
-      <StyledCreatePageTitle>커뮤니티 글 쓰기</StyledCreatePageTitle>
-      <StyledCreatePageSubtitle>회원들과 정보를 공유해보세요.</StyledCreatePageSubtitle>
-      <StyledTitleInputWrapper>
+      <StyledTitleWrapper>
+        <StyledCreatePageTitle>커뮤니티 글 쓰기</StyledCreatePageTitle>
+        <StyledCreatePageSubtitle>회원들과 정보를 공유해보세요.</StyledCreatePageSubtitle>
+      </StyledTitleWrapper>
+      <StyledInputWrapper>
         <StyledTitle>제목</StyledTitle>
-        <Input type="text" placeholder="스터디 이름을 입력하세요." value={title} onChange={handleTitleChange} />
+        <StyledInput value={title} onChange={handleTitleChange} placeholder="스터디 이름을 입력하세요." />
+      </StyledInputWrapper>
 
-      </StyledTitleInputWrapper>
-
-      <StyledTextInputWrapper>
+      <StyledInputWrapper className="second-input-wrapper">
         <StyledTitle>내용</StyledTitle>
-        <Input type="text" value={content} onChange={handleContentChange} placeholder="스터디 설명을 입력하세요." />
-
-      </StyledTextInputWrapper>
+        <StyledTextarea value={content} onChange={handleContentChange} placeholder="스터디 설명을 입력하세요." />
+      </StyledInputWrapper>
 
       <StyledFileInputWrapper>
-        <StyledTitle>파일 첨부</StyledTitle>
-        <FileInput type="file" onChange={handleFileChange} placeholder="파일을 첨부하세요." />
+        <StyledFileInputContainer>
+          <StyledTitle>파일 첨부</StyledTitle>
+          <StyledSubtitle>파일을 첨부하세요.</StyledSubtitle>
+          <FileInput type="file" id="fileInput" onChange={handleFileChange} />
+          <StyledFileButton htmlFor="fileInput">파일찾기</StyledFileButton>
+        </StyledFileInputContainer>
+        <FileList>
+          {fileList.map((file, index) => (
+            <FileListItem key={index}>
+              <FileAttachment href={URL.createObjectURL(file)} download>
+                {file.name}
+              </FileAttachment>
+            </FileListItem>
+          ))}
+        </FileList>
       </StyledFileInputWrapper>
-      <StyledDelButton onClick={handleDelete}>삭제</StyledDelButton>
-      <StyledCreateButton onClick={handleSubmit}>글쓰기</StyledCreateButton>
 
+      <StyledDelButton onClick={handleDelete}>삭제</StyledDelButton>
+      <StyledFileButtonWrapper>
+        <StyledCreateButton onClick={handleSubmit}>글쓰기</StyledCreateButton>
+      </StyledFileButtonWrapper>
     </StyledCreatePageContainer>
   );
 };
@@ -62,43 +76,165 @@ const CommunityCreatePage: React.FC = () => {
 const StyledCreatePageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center; /* 요소들을 가운데 정렬합니다 */
+  justify-content: center;
   align-items: center;
 `;
 
-const StyledCreatePageTitle = styled.h2`
+const StyledTitleWrapper = styled.div`
+  margin: 60px 0 25px;
+  width: 1270px;
+  display: flex;
+  align-items: flex-end; /* 폰트를 바닥에 같은 높이로 위치 */
+`;
+
+const StyledCreatePageTitle = styled.p`
   height: fit-content;
   font-family: 'establish Retrosans';
   color: #00E595;
-  font-size: 18px;
-  font-weight: bold;
-  margin-bottom: 10px;
+  font-size: 32px;
+  font-weight: 400;
+  margin: 0;
+  margin-right: 40px;
 `;
 
 const StyledCreatePageSubtitle = styled.p`
-  font-size: 14px;
-  margin-bottom: 20px;
+  font-weight: 300;
+  font-size: 18px;
+  margin: 0;
 `;
 
 const StyledTitle = styled.div`
-  
+  height: fit-content;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const StyledInput = styled.input`
+  width: 1107px;
+  height: 45px;
+  border: 1px solid #00057D;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: 300;
+  box-sizing: border-box;
+  line-height: 45px;
+  padding-left: 20px;
+
+  &::placeholder {
+    color: #C0C3E5;
+  }
+`;
+const StyledTextarea = styled.textarea`
+  width: 1107px;
+  height: 400px;
+  border: 1px solid #00057D;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: 300;
+  box-sizing: border-box;
+  padding: 20px;
+  resize: none;
+
+  &::placeholder {
+    color: #C0C3E5;
+  }
+`;
+
+const StyledInputWrapper = styled.div`
+  margin-bottom: 20px;
+  width: 1270px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  /* &.second-input-wrapper input[type="text"] {
+    height: 400px;
+    padding: 20px;
+  } */
+  .second-input-wrapper {
+    height: 400px;
+    position: relative;
+
+    ${StyledInput} {
+      position: absolute;
+      top: -20px;
+      left: 0;
+    }
+  }
+`;
+
+const StyledFileInputContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 const StyledFileInputWrapper = styled.div`
-`;
-const StyledTextInputWrapper = styled.div`
+  width: 1270px;
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+
+  ${StyledTitle} {
+    margin-right: 77px;
+    width: max-content;
+    display: inline-block;
+  }
 `;
 
-const StyledTitleInputWrapper = styled.div`
-    
+const StyledSubtitle = styled.p`
+  margin: 0;
+  width: 960px;
+  height: 45px;
+  border: 1px solid #00057D;
+  border-radius: 10px;
+  color: #C0C3E5;
+  font-size: 18px;
+  font-weight: 300;
+  box-sizing: border-box;
+  line-height: 45px;
+  padding-left: 20px;
 `;
 
-const Input = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
+const FileInput = styled.input`
+  display: none;
 `;
 
+const StyledFileButton = styled.label`
+  width: 132px;
+  height: 45px;
+  font-size: 18px;
+  font-weight: 600;
+  background-color: #2E3057;
+  border-radius: 10px;
+  text-align: center;
+  line-height: 45px;
+  color: #fff;
+  cursor: pointer;
+  margin-left: 15px;
+`;
+
+const FileList = styled.ul`
+  border: 1px solid #ddd;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  margin-top: 15px;
+`;
+
+const FileListItem = styled.li`
+  margin-top: 15px;
+`;
+
+const FileAttachment = styled.a`
+  display: block;
+`;
+
+const StyledFileButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 1270px;
+`
 const StyledCreateButton = styled.button`
   padding: 10px 20px;
   background-color: #00E595 ;
@@ -108,6 +244,8 @@ const StyledCreateButton = styled.button`
   color: #00057D;
   border: none;
   cursor: pointer;
+  margin-left: auto;
+  margin-bottom: 80px;
 `;
 
 const StyledDelButton = styled.button`
@@ -116,12 +254,10 @@ const StyledDelButton = styled.button`
   font-weight: 300;
   border: none;
   background-color: #ffffff;
-  
+  display: flex;
+  justify-content: flex-end;
+  width: 1270px;
+  margin-bottom: 15px;
 `
-
-const FileInput = styled.input`
-  margin-bottom: 10px;
-`;
-
 
 export default CommunityCreatePage;
