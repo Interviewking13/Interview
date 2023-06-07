b9f717b316b7e46db9e99865a5a6a6c471af24e2
frefloww <frefloww@gmail.com>
2023-06-07 13:51:22 +0900

    Fix : 파일업로드 수정 (추가수정필요)

--- public/index.js
+++ public/index.js
@@ -1,28 +1,38 @@
-import { axios } from 'axios';
-
+const fileInput = document.querySelector('input[type="file"]');
 const fileUploadBtn = document.querySelector('button');
-document.addEventListener('click', async () => {
-  
-  const upload = async (uploadFile, dir) => {
-    console.log('여기여기');
-    
-    if (uploadFile) {
-        const formData = new FormData();
-        formData.append('dir', dir);
-        formData.append('file', uploadFile);
 
-        await axios
-            .post('/api/upload', formData)
-            .then((response) => {
-                if (response.status === 200) {
-                    data = response.data;
-                }
-            })
-            .catch(err => {
-              console.log(JSON.stringify(err));
-          });
+const fileUpload = async (uploadFile, dir) => {
+  console.log('여기여기');
+  
+  if (uploadFile) {
+    const formData = new FormData();
+    formData.append('dir', dir);
+    formData.append('file', uploadFile);
+
+    try {
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        credentials: 'include',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('client data: ', data);
+      } else {
+        console.log('파일전송 실패');
       }
-      return data;
+    } catch (err) {
+      console.log(err);
     }
-  await upload(uploadFile, '/profile');
+  }
+};
+
+fileUploadBtn.addEventListener('click', async () => {
+  const uploadFile = fileInput.files[0];
+  await fileUpload(uploadFile, '/profile');
+});
+
+fileInput.addEventListener('change', () => {
+  // 파일 선택 후의 추가 로직
 });
--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,48 +1,60 @@
 const { Router } = require('express');
 const router = Router();
 
-const AWS = require('aws-sdk');
-const AWS_ACCESS = process.env.AWS_ACCESS;
-const AWS_SECRET = process.env.AWS_SECRET;
-const AWS_REGION = process.env.AWS_REGION;
-
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const multer = require('multer');
-const multerS3 = require('multer-s3');
+const { Readable } = require('stream');
 const moment = require('moment');
 
-const s3 = new AWS.S3({
-    accessKeyId: AWS_ACCESS,
-    secretAccessKey: AWS_SECRET,
-    region: AWS_REGION
-})
-
-const storage = multerS3({
-    s3: s3,
-    acl: 'public-read-write',
-    bucket: "13team",
-    key: (req, file, callback) => {
-    	let dir = req.body.dir;
-        let datetime = moment().format('YYYYMMDDHHmmss');
-        callback(null, dir + datetime + "_" + file.originalname);  // 저장되는 파일명
-    }
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
 });
 
-const upload = multer({ storage: storage });
+const upload = async (stream, filename, dir) => {
+  const datetime = moment().format('YYYYMMDDHHmmss');
+  const key = `${dir}/${datetime}_${filename}`;
 
-/** 파일업로드 */
-router.post('/upload', upload.single('file'), async (req, res) => {
-    try {
+  const params = {
+    Bucket: '13team',
+    Key: key,
+    Body: stream,
+    ACL: 'public-read-write',
+  };
 
-        let saveFile = req.file;
-        let params = [saveFile.originalname, saveFile.key, saveFile.location, saveFile.contentType];
+  const command = new PutObjectCommand(params);
 
-        console.log(saveFile);
-        console.log(params);
+  try {
+    await s3Client.send(command);
+    return key;
+  } catch (error) {
+    console.log(error);
+    throw new Error('파일업로드 실패');
+  }
+};
 
-    } catch {
-        console.log(err);
-        throw new Error(err);
-    }
+const storage = multer.memoryStorage();
+const uploadMiddleware = multer({ storage }).single('file');
+
+/** 파일 업로드 */
+router.post('/', uploadMiddleware, async (req, res) => {
+  try {
+    const file = req.file;
+    const fileStream = Readable.from(file.buffer); // 버퍼에서 Readable 스트림 생성
+    const uploadedKey = await upload(fileStream, file.originalname, req.body.dir);
+
+    console.log('upload router');
+    console.log(file);
+    console.log(uploadedKey);
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: '파일업로드 실패' });
+  }
 });
 
 module.exports = router;
