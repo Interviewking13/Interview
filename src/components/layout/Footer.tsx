000839fe9902993ce80933c353350322c20730f4
[SW4/2반] 공지아 <timing1018@gmail.com>
2023-06-09 16:50:35 +0900

    레이아웃 수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,11 +4,14 @@ import { colors } from "../../constants/colors";
 import { TitleText } from "../../constants/fonts";
 
 const StyledFooterWrapper = styled.div`
+  position: sticky;
+  bottom: 0;
   width: 100%;
   background-color: ${colors.back_navy};
   padding-bottom: 50px;
 `
 const Divider = styled.div`
+  margin-top: 35px;
   margin-bottom: 40px;
   border-bottom: 1px solid ${colors.gray_stroke};
 `;
