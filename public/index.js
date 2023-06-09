b59740d51c9c8b584e433819563853fb61b30669
frefloww <frefloww@gmail.com>
2023-06-09 18:14:59 +0900

    Fix : 파일업로드 미들웨어로 구성

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 const fileInput = document.querySelector('input[type="file"]');
 const fileUploadBtn = document.querySelector('#upload');
-const fileDownloadBtn = document.querySelector('#download');
+const fileDownloadText = document.querySelector('#download');
 
 fileUploadBtn.addEventListener('click', async () => {
   const uploadFile = fileInput.files[0];
@@ -12,10 +12,8 @@ fileInput.addEventListener('change', () => {
   alert('파일첨부 완료');
 });
 
-fileDownloadBtn.addEventListener('click', async () => {
-  const uploadFile = fileInput.files[0];
-  console.log('client uploadFile: ', uploadFile);
-  await downloadFile(uploadFile, '/community');
+fileDownloadText.addEventListener('click', async () => {
+  await downloadFile('b4340c822d88e525bc82b8a93731aa6e');
 });
 
 //파일업로드
@@ -27,7 +25,7 @@ const fileUpload = async (uploadFile, dir) => {
     formData.append('file', uploadFile);
 
     try {
-      const response = await fetch('/api/upload', {
+      const response = await fetch('/api/community/detl', {
         method: 'POST',
         withCredentials: true,
         body: formData,
@@ -47,11 +45,13 @@ const fileUpload = async (uploadFile, dir) => {
 
 //다운로드
 async function downloadFile(fileId) {
-  
+  const fileName = '20230609165522_README.md';
+
   // 서버로 파일 다운로드 요청을 보내고 파일 URL을 받아옴
-  const response = await fetch('/api/upload/:fileId', {
+  const response = await fetch(`/api/community/detl?community_no=`, {
     method: 'GET',
     withCredentials: true,
+    body: fileId, fileName
   }).then(response => {
     // 파일 다운로드 링크 생성
     const downloadLink = document.createElement('a');
---
+++ src/middlewares/fileUpload.js
@@ -0,0 +1,51 @@
+const { Router } = require('express');
+const router = Router();
+
+const multer = require('multer');
+const { PutObjectCommand } = require('@aws-sdk/client-s3');
+const { Readable } = require('stream');
+const moment = require('moment');
+const s3 = require('../config/s3');
+
+/** 파일 업로드 */
+async function fileUpload(req, res, next) {
+    try {
+
+        /** 서버로 업로드 */
+        const storage = multer.memoryStorage();
+        const uploadMiddleware = multer({ storage }).single('file');
+
+        uploadMiddleware(req, res, async (err) => {
+            if (err) {
+                console.log(err);
+                res.status(400).json({ message: '파일업로드 실패' });
+                return;
+            }
+        
+        /** 서버에서 S3로 업로드 */
+        const fileStream = Readable.from(req.file.buffer);
+        const datetime = moment().format('YYYYMMDDHHmmss');
+        const key = `${req.body.dir}/${datetime}_${req.file.originalname}`;
+  
+        const params = {
+          Bucket: '13team',
+          Key: key,
+          Body: fileStream,
+          ContentLength: req.file.size,
+        };
+  
+        const command = new PutObjectCommand(params);
+        const response = await s3.send(command);
+  
+        if(response){
+             req.ETag = response.ETag;
+             next();
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ message: '파일업로드 실패' });
+    }
+}
+
+module.exports = fileUpload;
