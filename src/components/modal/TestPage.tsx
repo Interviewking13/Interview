1ec0720ee1a064b4180ab7e78d3f99ba067d6990
고병욱 <rhquddnr9502@gmail.com>
2023-06-09 15:27:31 +0900

    Docs : 풀을 위한 커밋

--- src/components/modal/TestPage.tsx
+++ src/components/modal/TestPage.tsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StudyApplyModal from "./StudyApplyModal";
 
 // {/* 디자인 적용 예시 */}
 import styled from "styled-components";
 import { TitleText } from "../../constants/fonts";
 import { colors } from "../../constants/colors";
+import { useQuery } from "react-query";
 // {/* 디자인 적용 예시 */}
 
 const TestPage: React.FC = () => {
+  // const { data: studyData, isLoading } = useQuery(["orders"], () =>
+  //   getStudyData()
+  // );
+
+  const handleFetchStudyData = async () => {
+    try {
+      // const studyData = await getStudyData();
+      // console.log(studyData);
+      // 여기서 studyData를 원하는 방식으로 활용할 수 있습니다.
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  handleFetchStudyData();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => {
