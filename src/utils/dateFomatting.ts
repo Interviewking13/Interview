79bfb1b82816c7d002fa1ad3617234bff4c72ec8
Lee yong sub <dydtjq324@naver.com>
2023-06-09 16:38:53 +0900

    as

--- src/utils/dateFomatting.ts
+++ src/utils/dateFomatting.ts
@@ -1,3 +1,13 @@
 export const dateFomatting = (date: string) => {
   return date.replaceAll("-", ".");
 };
+
+export const dateSplice = (input: string) => {
+  const dateString = input;
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // getMonth()의 반환값은 0부터 시작하므로 +1 해줍니다.
+  const day = date.getDate();
+  const formattedDate = `${year}년 ${month}월 ${day}일`;
+  return formattedDate;
+};
