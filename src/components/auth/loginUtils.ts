dbceb042b7484e97ce4a4dccfc4faab0e10efbec
[SW4/2반] 공지아 <timing1018@gmail.com>
2023-06-11 16:57:16 +0900

    쿠키 만료 설정 변경

--- src/components/auth/loginUtils.ts
+++ src/components/auth/loginUtils.ts
@@ -2,7 +2,8 @@
 
 export const storeTokenInCookie = (token: string) => {
   const expires = new Date();
-  expires.setDate(expires.getDate() + 7); // 쿠키 만료 7일 후로 설정
+  // expires.setDate(expires.getDate() + 7); // 쿠키 만료 7일 후로 설정
+  expires.setHours(expires.getHours() + 24); // 쿠키 만료 24시간 후로 설정
 
   const cookie = `token=${token}; expires=${expires.toUTCString()}; path=/`;
   document.cookie = cookie;
