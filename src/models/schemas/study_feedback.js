660b0c5cf110e14900ab61a001e9b572e5aacaac
merry <merrykang1103@gmail.com>
2023-06-12 09:47:01 +0900

    [Fix] 스터디 api, 스터디 피드백 api 기본 기능 최종 수정 및 로컬 테스트 완료

--- src/models/schemas/study_feedback.js
+++ src/models/schemas/study_feedback.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const StudyFeedbackSchema = new Schema(
   {
-    study_id: { type: mongoose.Types.ObjectId, unique: true, ref: 'Study' }, // reference
-    user_id: { type: mongoose.Types.ObjectId, unique: true, ref: 'User' }, // reference
+    study_id: { type: mongoose.Types.ObjectId, ref: 'Study' }, // reference
+    user_id: { type: mongoose.Types.ObjectId, ref: 'User' }, // reference
     content_type: { type: Boolean }, // 0: 피드백 본문, 1: 피드백 댓글
     content: { type: String },
   },
