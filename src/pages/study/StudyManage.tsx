6dce943ac26555fac278bdd4357d19bb1d0c8d88
고병욱 <rhquddnr9502@gmail.com>
2023-06-15 14:21:23 +0900

    Feat : 관리 메인 추가 및 텝 ui 완료

--- src/pages/study/StudyManage.tsx
+++ src/pages/study/StudyManage.tsx
@@ -5,15 +5,19 @@ import { useNavigate } from "react-router-dom";
 import StudyModify from "../../components/study/manage/StudyModify";
 import StudyMemberManagement from "../../components/study/manage/StudyMemberManagement";
 import StudyApplicantList from "../../components/study/manage/StudyApplicantList";
+
+import StudyManageMain from "../../components/study/manage/StudyManageMain";
 import styled from "styled-components";
 import { colors } from "../../constants/colors";
 import * as fonts from "../../constants/fonts";
 import { Link } from "react-router-dom";
 import MenuTapBtn from "../../components/UI/MenuTapBtn";
 const StudyManage = () => {
-  const navigate = useNavigate();
-  const [activePage, setActivePage] = useState("");
+  const [activePage, setActivePage] = useState("manage-main");
 
+  const onClickStudyManageMain = () => {
+    setActivePage("manage-main");
+  };
   const onClickStudyModify = () => {
     setActivePage("modify");
   };
@@ -25,31 +29,52 @@ const StudyManage = () => {
   const onClickApplicantList = () => {
     setActivePage("applicant");
   };
+  type StudyManageMainProps = {
+    activePage: string;
+    onClickStudyModify: () => void;
+    onClickMemberManagement: () => void;
+    onClickApplicantList: () => void;
+  };
+  const studyManageMainProps: StudyManageMainProps = {
+    activePage,
+    onClickStudyModify,
+    onClickMemberManagement,
+    onClickApplicantList,
+  };
 
   return (
     <div>
       <CommonContainer>
         <StyledStudyListTopArea>
-          <StyledTitleText>스터디 관리</StyledTitleText>
-          <StyledSubTextThin>스터디 관리자 페이지입니다.</StyledSubTextThin>
+          <StyledTitleText onClick={onClickStudyManageMain}>
+            스터디 관리
+          </StyledTitleText>
+          <StyledSubTextThin>스터디를 관리하세요.</StyledSubTextThin>
         </StyledStudyListTopArea>
         {/* 버튼1, 버튼2, 버튼3 */}
-        <Grid item xs={12}>
-          <Grid container spacing={1} justifyContent="flex-start">
-            <MenuTapBtn onClick={onClickStudyModify}>
-              <Create />
-              정보수정
-            </MenuTapBtn>
-            <MenuTapBtn onClick={onClickMemberManagement}>
-              <Checklist />
-              회원관리
-            </MenuTapBtn>
-            <MenuTapBtn onClick={onClickApplicantList}>
-              <Person />
-              신청목록
-            </MenuTapBtn>
-          </Grid>
-        </Grid>
+        {activePage === "manage-main" ? (
+          <StudyManageMain {...studyManageMainProps} />
+        ) : (
+          <>
+            <Grid item xs={12}>
+              <Grid container spacing={1} justifyContent="flex-start">
+                <MenuTapBtn onClick={onClickStudyModify}>
+                  <Create />
+                  정보수정
+                </MenuTapBtn>
+                <MenuTapBtn onClick={onClickMemberManagement}>
+                  <Checklist />
+                  회원관리
+                </MenuTapBtn>
+                <MenuTapBtn onClick={onClickApplicantList}>
+                  <Person />
+                  신청목록
+                </MenuTapBtn>
+              </Grid>
+            </Grid>
+          </>
+        )}
+
         {activePage === "modify" && <StudyModify />}
         {activePage === "member" && <StudyMemberManagement />}
         {activePage === "applicant" && <StudyApplicantList />}
@@ -73,6 +98,7 @@ const StyledStudyListTopArea = styled.div`
 `;
 const StyledTitleText = styled.p`
   height: fit-content;
+  cursor: pointer;
   ${fonts.TitleText}
   color: ${colors.main_mint};
   margin: 0 30px 0 0;
