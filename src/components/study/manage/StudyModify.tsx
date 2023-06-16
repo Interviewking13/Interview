fc294cf74ab38d7cd4a7362a7242f6234951ea6c
고병욱 <rhquddnr9502@gmail.com>
2023-06-16 17:17:41 +0900

    Feat : api 토큰 가져와서 삭제 요청 구현

--- src/components/study/manage/StudyModify.tsx
+++ src/components/study/manage/StudyModify.tsx
@@ -3,10 +3,62 @@ import styled from "styled-components";
 import { colors } from "../../../constants/colors";
 import * as fonts from "../../../constants/fonts";
 import { Link } from "react-router-dom";
+import { deleteStudy, getInfoStudyData } from "../../../api/api-study";
+import { useQuery } from "react-query";
+import React, { useState, useEffect } from "react";
+import { dateFomatting, dateFomattingLine } from "../../../utils/dateFomatting";
+
 interface StudyModifyProps {
   studyId: string;
 }
 const StudyModify: React.FC<StudyModifyProps> = ({ studyId }) => {
+  const {
+    data: studyData,
+    isLoading,
+    isError,
+  } = useQuery(["studyData"], () =>
+    getInfoStudyData(studyId).then((response) => response.data)
+  );
+  const [studyName, setStudyName] = useState(""); // 스터디 이름 상태 추가
+  const [studyDescription, setStudyDescription] = useState(""); // 스터디 설명 상태 추가
+  const [meetingLink, setMeetingLink] = useState(""); // 회의 링크 상태 추가
+  const [startDate, setStartDate] = useState(""); // 시작 날짜 상태 추가
+  const [endDate, setEndDate] = useState(""); // 종료 날짜 상태 추가
+  const [recruitmentDeadline, setRecruitmentDeadline] = useState(""); // 모집 마감일 상태 추가
+  const [recruitmentCount, setRecruitmentCount] = useState(0); // 모집 인원 상태 추가
+
+  useEffect(() => {
+    if (studyData) {
+      const { title, content, chat_link, start, end, deadline, headcount } =
+        studyData;
+      console.log(dateFomattingLine(start));
+      setStudyName(title);
+      setStudyDescription(content);
+      setMeetingLink(chat_link);
+      setStartDate(dateFomattingLine(start));
+      setEndDate(dateFomattingLine(end));
+      setRecruitmentDeadline(dateFomattingLine(deadline));
+      setRecruitmentCount(headcount);
+    }
+  }, [studyData]);
+  if (isLoading) {
+    // 로딩 상태를 표시
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    // 에러 상태를 표시
+    return <div>Error occurred while fetching data</div>;
+  }
+  const handleModifyClick: React.MouseEventHandler<HTMLDivElement> = () => {
+    // 스터디 수정 로직을 처리하는 코드를 작성하세요.
+    // 입력된 상태값들을 사용하여 API 호출 등의 작업을 수행할 수 있습니다.
+  };
+  const handleDeleteClick: React.MouseEventHandler<HTMLDivElement> = () => {
+    // 스터디 삭제 로직을 처리하는 코드를 작성하세요.
+    // 필요한 API 호출 등의 작업을 수행할 수 있습니다.
+    deleteStudy(String(localStorage.getItem("token")), studyId);
+  };
   return (
     <>
       <StyledStudyCreateArea>
@@ -15,30 +67,50 @@ const StudyModify: React.FC<StudyModifyProps> = ({ studyId }) => {
           <StyledStudyInput
             type="text"
             placeholder="스터디 이름을 입력하세요."
+            value={studyName}
+            onChange={(e) => setStudyName(e.target.value)}
           />
         </StyledStudyCreateInputArea>
         <StyledStudyCreateInputAreaBig>
           <StyledStudyCreateText>스터디 소개</StyledStudyCreateText>
-          <StyledStudyInputBig placeholder="스터디 설명을 입력하세요." />
+          <StyledStudyInputBig
+            placeholder="스터디 설명을 입력하세요."
+            value={studyDescription}
+            onChange={(e) => setStudyDescription(e.target.value)}
+          />
         </StyledStudyCreateInputAreaBig>
         <StyledStudyCreateInputArea>
           <StyledStudyCreateText>회의 링크</StyledStudyCreateText>
           <StyledStudyInput
             type="url"
             placeholder="화상 회의 주소를 입력하세요."
+            value={meetingLink}
+            onChange={(e) => setMeetingLink(e.target.value)}
           />
         </StyledStudyCreateInputArea>
         <StyledStudyCreateInputArea>
           <StyledStudyCreateText>진행 기간</StyledStudyCreateText>
           <StyledDateArea>
-            <StyledStudyDate type="date" />
+            <StyledStudyDate
+              type="date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
             <StyledStudyDateText>~</StyledStudyDateText>
-            <StyledStudyDate type="date" />
+            <StyledStudyDate
+              type="date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
           </StyledDateArea>
         </StyledStudyCreateInputArea>
         <StyledStudyCreateInputArea>
           <StyledStudyCreateText>모집 마감일</StyledStudyCreateText>
-          <StyledStudyDate type="date" />
+          <StyledStudyDate
+            type="date"
+            value={recruitmentDeadline}
+            onChange={(e) => setRecruitmentDeadline(e.target.value)}
+          />
         </StyledStudyCreateInputArea>
         <StyledStudyCreateInputArea>
           <StyledStudyCreateText>모집 인원</StyledStudyCreateText>
@@ -46,19 +118,27 @@ const StudyModify: React.FC<StudyModifyProps> = ({ studyId }) => {
             type="number"
             min="1"
             placeholder="모집 인원을 입력하세요."
+            value={recruitmentCount}
+            onChange={(e) => setRecruitmentCount(Number(e.target.value))}
           />
         </StyledStudyCreateInputArea>
         <StyledStudyCreateBtnArea>
           <StyledLink to={`/study/modify`}>
             <SubButtonContainer>
-              <StyledCommonButton backgroundColor={colors.main_mint}>
+              <StyledCommonButton
+                backgroundColor={colors.main_mint}
+                onClick={handleModifyClick} // onClick 이벤트 핸들러 추가
+              >
                 <StyledButtonTextDelete>수정하기</StyledButtonTextDelete>
               </StyledCommonButton>
             </SubButtonContainer>
           </StyledLink>
           <StyledLink to={`/study/delete`}>
             <SubButtonContainer>
-              <StyledCommonButton backgroundColor={colors.main_red}>
+              <StyledCommonButton
+                backgroundColor={colors.main_red}
+                onClick={handleDeleteClick}
+              >
                 <StyledButtonTextDelete>스터디 삭제</StyledButtonTextDelete>
               </StyledCommonButton>
             </SubButtonContainer>
