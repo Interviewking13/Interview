f68d524fb35aa4f6a9d406130c318922916a9c91
고병욱 <rhquddnr95@naver.com>
2023-06-17 02:54:03 +0900

    Feat : 스터디 신청 수락 및 거절 완료

--- src/components/study/manage/StudyApplicantList.tsx
+++ src/components/study/manage/StudyApplicantList.tsx
@@ -6,65 +6,79 @@ import React, { useEffect, useState } from "react";
 import { Modal } from "@mui/material";
 import UserInfoModal from "../../modal/UserInfoModal";
 import { useQuery } from "react-query";
-import { getStudyAccept } from "../../../api/api-study";
+import { getStudyAccept, putAcceptStudy } from "../../../api/api-study";
 
 type StudyApplicantListProps = {
   studyId: string;
 };
 
-//변경 {/* 변경 */}
-const members = [
-  { name: "고병욱", description: "힘내자 힘", userId: "1232" },
-  { name: "이예준", description: "열심히 하자", userId: "1234" },
-  { name: "박지원", description: "화이팅!", userId: "1235" },
-];
+interface StudyAcceptData {
+  _id: string;
+  study_id: string;
+  user_id: string;
+  user_name: string;
+  is_leader: boolean;
+  goal: string;
+  accept: number;
+}
 
 const StudyApplicantList = ({ studyId }: StudyApplicantListProps) => {
-  const accept = 0;
-  const unAccept = 1;
+  const accept = 1;
+  const unAccept = 2;
   // {/* 변경 */}
-  // const onAcceptButton = async () => {
-  //   await putAcceptStudy(String(localStorage.getItem("token")),
-  //     study_id,
-  //     member_id,
-  //     accept);
-  // };
-  // const onDeleteButton = async () => {
-  //   await putAcceptStudy(String(localStorage.getItem("token")),
-  //   study_id,
-  //   member_id,
-  //   unAccept);
-  // };
-  const onAcceptButton = () => {};
-  const onDeleteButton = () => {};
-  getStudyAccept(studyId, accept).then((res) => {
-    console.log("1234");
-  });
-  console.log(String(localStorage.getItem("token")));
-  console.log(studyId);
+  const onAcceptButton = async (index: number) => {
+    const userId = studyAcceptData[index].user_id;
+    console.log(
+      `putAcceptStudy의 헤더와 바디는,
+      token:${String(localStorage.getItem("token"))},
+      studyId:${studyId},
+      userId:${userId},
+      accept:${accept}`
+    );
+    await putAcceptStudy(
+      String(localStorage.getItem("token")),
+      studyId,
+      userId,
+      accept
+    );
+  };
+  const onDeleteButton = async (index: number) => {
+    const userId = studyAcceptData[index].user_id;
+    await putAcceptStudy(
+      String(localStorage.getItem("token")),
+      studyId,
+      userId,
+      unAccept
+    );
+  };
+  // const onAcceptButton = () => {};
+  // const onDeleteButton = () => {};
+  // getStudyAccept(studyId, accept).then((res) => {
+  //   console.log("1234");
+  // });
+  // console.log(String(localStorage.getItem("token")));
+  // console.log(studyId);
 
   // {/* 변경 */}
-  // const {
-  //   data: studyAcceptData,
-  //   isLoading,
-  //   isError,
-  // } = useQuery(["studyAcceptData"], () =>
-  //   getStudyAccept( studyId, 0).then(
-  //     (response) => response.data
-  //   )
-  // );
-  // if (isLoading) {
-  //   // 로딩 상태를 표시
-  //   return <div>Loading...</div>;
-  // }
+  const {
+    data: studyAcceptData,
+    isLoading,
+    isError,
+  } = useQuery(["studyAcceptData"], () =>
+    getStudyAccept(studyId, 0).then((response) => response.data)
+  );
+  if (isLoading) {
+    // 로딩 상태를 표시
+    return <div>Loading...</div>;
+  }
 
-  // if (isError) {
-  //   // 에러 상태를 표시
-  //   return <div>Error occurred while fetching data</div>;
-  // }
+  if (isError) {
+    // 에러 상태를 표시
+    return <div>Error occurred while fetching data</div>;
+  }
 
-  // console.log(`studyAcceptData:`, studyAcceptData);
-  // const { user_id, user_name,goal } = studyAcceptData;
+  console.log(`studyAcceptData:`, studyAcceptData);
+  const members = studyAcceptData;
   const [userInfoModalOpen, setUserInfoModalOpen] = React.useState(false);
 
   const handleOpenUserInfoModal = () => {
@@ -76,32 +90,32 @@ const StudyApplicantList = ({ studyId }: StudyApplicantListProps) => {
   };
   return (
     <>
-      {members.map((member, index) => (
+      {members.map((member: StudyAcceptData, index: number) => (
         <CardContainer key={index}>
           <CardContent>
             <StyledName onClick={handleOpenUserInfoModal}>
               {/* 변경 */}
-              {member.name}
+              {member.user_name}
             </StyledName>
             <Modal open={userInfoModalOpen} onClose={handleCloseUserInfoModal}>
               <UserInfoModal
                 // {/* 변경 */}
-                userId={member.userId}
+                userId={member.user_id}
                 handleModalClose={handleCloseUserInfoModal}
               />
             </Modal>
             {/* 변경 */}
-            <StyledDescription>{member.description}</StyledDescription>
+            <StyledDescription>{member.goal}</StyledDescription>
           </CardContent>
           <StyledCommonButton
             backgroundColor={colors.main_mint}
-            onClick={onAcceptButton}
+            onClick={() => onAcceptButton(index)}
           >
             신청 수락
           </StyledCommonButton>
           <StyledCommonButton
             backgroundColor={colors.main_red}
-            onClick={onDeleteButton}
+            onClick={() => onDeleteButton(index)}
           >
             회원 거절
           </StyledCommonButton>
