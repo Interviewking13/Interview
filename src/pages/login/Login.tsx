dc348f240c5225262e2eb4f5b65206d051a1dc6e
[SW4/2반] 공지아 <timing1018@gmail.com>
2023-06-30 23:45:38 +0900

    Refactor : 로그인 주석 및 코드컨벤션 수정

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -7,6 +7,7 @@ import LeftSignContainer from "../../components/auth/LeftSignContainer";
 import { postSignIn } from "../../api/api-user";
 import { useMutation, useQueryClient } from "react-query";
 
+/** refactor */
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
