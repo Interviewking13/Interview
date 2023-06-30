32a9f6889b03cc26122f9cc01a074979961fe30f
uniqueflowSA <saco2020a@gmail.com>
2023-07-01 01:52:05 +0900

    Refactor_Design_userSide

--- src/components/mypage/UserInfo.tsx
+++ src/components/mypage/UserInfo.tsx
@@ -9,150 +9,83 @@ import * as fonts from "../../constants/fonts";
 import { colors } from "../../constants/colors";
 import { useQuery } from "react-query";
 import { getUserData } from "../../api/api-user";
-import AWS from "aws-sdk";
-
-//1. s3 스토리지 플러그인가져오기
-AWS.config.update({
-  //스토리지인증키작성
-  accessKeyId: "AKIA4WQLMJXFZI2K7J2F",
-  secretAccessKey: "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",
-  region: "ap-northeast-2",
-});
 
 const UserInfo = () => {
-  const navigate = useNavigate();
-  const onClickModify = () => {
-    navigate("/mypage/userinfo/Modify"); // useNavigate 사용하여 페이지 이동
-  };
-  const token =
-    localStorage.getItem("token") || ""; /**회원정보조회를 위한 토큰 가져오기*/
-  const {
-    data: userData,
-    isLoading,
-    isError,
-  } = useQuery(["userData"], () => getUserData(token as string)); //useQuery로 getdata
-
-  // token 값을 활용하여 필요한 작업을 수행
-  console.log("UserData", userData);
-  const { user_name, phone_number, email, file_key, file_name } =
-    userData?.data || {};
-
-  const onClickfileDownload = () => {
-    const s3 = new AWS.S3();
-    const bucketName = "13team";
-
-    const params = {
-      Bucket: bucketName,
-      Key: file_key,
-    };
-
-    s3.getSignedUrl("getObject", params, (err, url) => {
-      if (err) {
-        console.error("Error generating download URL:", err);
-        return;
-      }
-      console.log("Download URL:", url);
-      // 생성된 다운로드 URL을 사용하거나, 이를 표시할 다이얼로그 또는 링크로 전달하여 사용자에게 제공합니다.
-      window.open(url, "_blank");
-    });
-  };
-  if (isLoading) {
-    // 로딩 상태를 표시
-    return <div>Loading...</div>;
-  }
-  if (isError) {
-    // 에러 상태를 표시
-    return <div>Error occurred while fetching token</div>;
-  }
-
   return (
     <StyledDiv>
-      <Grid container rowSpacing={2} alignItems={"center"}>
-        <Grid item xs={11.5}>
-          <StyledSayHello noWrap>
-            면접왕 <b>{user_name}</b> 님, 어서오세요.
-          </StyledSayHello>
-        </Grid>
-        <Grid item xs={0.5}>
-          <IconButton onClick={onClickModify}>
-            <SettingsIcon />
-          </IconButton>
-          {/* 설정 버튼 UI */}
-        </Grid>
-      </Grid>
-      <StyledLine></StyledLine>
-      {/* 구분선 */}
-      <StyledInfo container rowSpacing={2.5}>
-        <Grid item xs={12}>
-          <Grid container>
-            <Grid item xs={2}>
-              <StyledInfoText>아이디</StyledInfoText>
-            </Grid>
-            <Grid item>
-              <StyledInfoValue> {email}</StyledInfoValue>
-            </Grid>
-          </Grid>
-        </Grid>
+      <StyledLowContent>
+        <StyledWelcome noWrap>면접왕 님, 어서오세요.</StyledWelcome>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+      </StyledLowContent>
 
-        <Grid item xs={12}>
-          <Grid container>
-            <Grid item xs={2}>
-              <StyledInfoText>연락처</StyledInfoText>
-            </Grid>
-            <Grid item>
-              <StyledInfoValue>01023445678</StyledInfoValue>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container>
-            <Grid item xs={2}>
-              <StyledInfoText>자기소개서</StyledInfoText>
-            </Grid>
-            <Grid item>
-              <button onClick={onClickfileDownload}>{file_name}</button>
-            </Grid>
-          </Grid>
-        </Grid>
+      {/* 구분선 */}
+      <StyledLine></StyledLine>
+      {/* 회원정보부분 */}
+      <StyledInfo>
+        <StyledLowContent>
+          <StyledInfoText>아이디</StyledInfoText>
+          <StyledInfoValue>면접왕</StyledInfoValue>
+        </StyledLowContent>
+
+        <StyledLowContent>
+          <StyledInfoText>연락처</StyledInfoText>
+          <StyledInfoValue>01023445678</StyledInfoValue>
+        </StyledLowContent>
+
+        <StyledLowContent>
+          <StyledInfoText>자기소개서</StyledInfoText>
+          <button>파일 버튼 텍스트 버튼</button>
+        </StyledLowContent>
       </StyledInfo>
     </StyledDiv>
   );
 };
 
 export default UserInfo;
-
-const StyledSayHello = styled(Typography)`
+/** 인삿말 스타일:===님 환영합니다. */
+const StyledWelcome = styled(Typography)`
   && {
     ${fonts.SubTextBig};
     color: ${colors.main_navy};
     margin: 22px;
   }
 `;
-
-const StyledDiv = styled(Box)`
-  && {
-    border: 1px solid ${colors.main_navy};
-    border-radius: 7px;
-  }
+/** 배경 테두리 */
+const StyledDiv = styled.div`
+  border: 1px solid ${colors.main_navy};
+  border-radius: 7px;
 `;
-
-const StyledInfo = styled(Grid)`
-  && {
-    margin: 10px 0 22px 22px;
-  }
-`;
+/** 구분선 */
 const StyledLine = styled(Divider)`
   && {
     border: 1px solid ${colors.main_navy};
   }
 `;
+/** 테두리 내 여백 */
+const StyledInfo = styled.div`
+  margin: 10px 0 22px 22px;
+`;
 
+/** 각 요소 가로배열 */
+const StyledLowContent = styled.div`
+  margin-top: 15px;
+  display: flex;
+  flex-direction: low;
+  align-items: center;
+  width: 1269px;
+`;
+
+/** 회원정보 요소 텍스트 스타일 */
 const StyledInfoText = styled(Typography)`
   && {
     color: ${colors.darkgray_navy};
     ${fonts.SubTextThinSmall};
+    width: 180px;
   }
 `;
+/** 회원정보 텍스트 스타일 */
 const StyledInfoValue = styled(Typography)`
   && {
     color: ${colors.main_navy};
