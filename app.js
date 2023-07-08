97221b767ec9e1c5b35d3a194649518c231987cd
cobaltcyan <cobaltcyan.park@gmail.com>
2023-07-09 03:34:13 +0900

    [Fix] gitignore 파일 수정에 따른 관련 파일 수정

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '.env' });
 
 const express = require('express');
 const app = express();
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
 // Router
 const indexRouter = require('./src/routes/index');
@@ -17,9 +17,6 @@ app.use(express.static('public'));
 app.use('/', indexRouter);
 
 // PORT
-app.listen();
-
-// PORT
-// app.listen(PORT, () => {
-  // console.log(`Server on http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server on http://localhost:${PORT}`);
+});
