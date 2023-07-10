b7db5120ca967838ff9250b9f9305f2bf1e3cae4
rhquddnr95 <39820865+rhquddnr95@users.noreply.github.com>
2023-07-11 02:20:44 +0900

    Refactor : 현재 날짜 가져오는 함수 작성

---
+++ src/utils/getCurrentDate.ts
@@ -0,0 +1,8 @@
+/** 오늘 날짜를 반환하는 함수 */
+export const getCurrentDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+};
