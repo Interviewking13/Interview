c738c5efb22b9ace5680ecf9824e720c6496eee2
uniqueflowSA <saco2020a@gmail.com>
2023-07-12 18:02:18 +0900

    refactor UserStudy_APIconnect_propsSetting

--- src/components/mypage/UserStudy.tsx
+++ src/components/mypage/UserStudy.tsx
@@ -7,37 +7,56 @@ import { colors } from "../../constants/colors";
 import axios from "axios";
 import StudyManageList from "../UI/StudyManageList";
 
-//각 페이지컴포넌트 호출
-
 const UserStudy = () => {
-  const [makedStudy, setMakedStudy] = useState([]);
-  const [enteredStudy, setEnteredStudy] = useState([]);
+  const [studyList, setStudyList] = useState<any[]>([]);
+
+  // const handleLeaveStudy = () => {};
+  // const handle;
 
   const getData = () => {
-    const res = axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => {});
-    const initData = res.slice(0, 20).map((item) => {
-      return {
-        title: item.title,
-        manager: userId,
-        key: id,
-      };
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      const initData = res.data.slice(0, 20).map((item: any) => {
+        return {
+          title: item.title,
+          manager: item.userId,
+          id: item.id,
+        };
+      });
+      setStudyList(initData);
     });
-    console.log(initData);
   };
   useEffect(() => {
     getData();
   }, []);
 
-  return (
-    <>
-      <StyledTitle>개설스터디</StyledTitle>
-      <StudyManageList />
+  const filterMakedStudy = studyList.filter((item: any) => item.id >= 13);
+  const filterEnteredStudy = studyList.filter((item: any) => item.id < 13);
 
-      <StyledTitle>참여스터디</StyledTitle>
-      <StudyManageList />
-    </>
+  return (
+    <StyledContainer>
+      <StyledTitle>개설스터디</StyledTitle>
+      {filterMakedStudy.map((item) => (
+        <StudyManageList
+          id={item.id}
+          title={item.title}
+          manager={item.manager}
+          backgroundColor={colors.main_mint}
+          children={"스터디 관리"}
+        />
+      ))}
+      <StyledContent>
+        <StyledTitle>참여스터디</StyledTitle>
+      </StyledContent>
+      {filterEnteredStudy.map((item) => (
+        <StudyManageList
+          id={item.id}
+          title={item.title}
+          manager={item.manager}
+          backgroundColor={colors.main_red}
+          children={"스터디 탈퇴"}
+        />
+      ))}
+    </StyledContainer>
   );
 };
 
@@ -48,3 +67,11 @@ const StyledTitle = styled(Typography)`
   }
 `;
 export default UserStudy;
+
+const StyledContainer = styled.div`
+  width: 1270px;
+  margin-bottom: 200px;
+`;
+const StyledContent = styled.div`
+  margin-top: 20px;
+`;
