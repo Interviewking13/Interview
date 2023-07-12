e2edafc3a2d03e7657a1ea1ac448702e2bb705ac
[SW4/2반] 공지아 <timing1018@gmail.com>
2023-07-13 03:09:16 +0900

    Feat: 헤더 기능 추가 및 로그인 아이/비밀번호 찾기 레이아웃 내용 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,43 @@
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { colors } from "../../constants/colors";
 import { TitleText, SubText, SubTextThinSmall } from "../../constants/fonts";
+import { getUserData } from "../../api/api-user";
 
 const Header = (): JSX.Element => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
+  const storedName = localStorage.getItem("name");
+
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    if (token && storedName) {
+      setUserName(storedName);
+    } else if (token) {
+      // 토큰이 있으면 사용자 정보를 가져와서 이름을 설정합니다.
+      getUserInfo();
+    }
+  }, [token, storedName]);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     navigate("/login");
+  };
+
+  const handleMyPageClick = () => {
+    alert("로그인이 필요합니다.\n로그인 페이지로 이동합니다.");
+  };
+
+  const getUserInfo = async () => {
+    try {
+      const userData = await getUserData(token!); // 토큰을 인자로 getUserData 함수를 호출하여 사용자 정보를 가져옵니다.
+      const userName = userData.data.user_name; // 응답 데이터에서 user_name 값을 추출합니다.
+      setUserName(userName + "님"); // 사용자 정보에서 이름을 추출하여 설정합니다.
+    } catch (error) {
+      console.log("사용자 정보를 가져오는 데 실패했습니다.", error);
+    }
   };
 
   return (
@@ -27,14 +55,14 @@ const Header = (): JSX.Element => {
             <>
               <StyledLoginItem to="/login">로그인</StyledLoginItem>
               <StyledLoginItem to="/login/signup">회원가입</StyledLoginItem>
-              <StyledLoginItem to="/login">마이페이지</StyledLoginItem>
+              <StyledLoginItem to="/login" onClick={handleMyPageClick}>마이페이지</StyledLoginItem>
             </>
           ) : (
             <>
               <StyledLogOutButton to="" onClick={handleLogout}>
                 로그아웃
               </StyledLogOutButton>
-              <StyledLoginItem to="/mypage">마이페이지</StyledLoginItem>
+              <StyledLoginItem to="/mypage">{userName ? userName : "마이페이지"}</StyledLoginItem>
             </>
           )}
         </StyledLoginItemContainer>
