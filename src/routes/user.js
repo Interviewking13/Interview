b97826fdec74d0c39bcde13be76a0fa1fba7ce5e
cobaltcyan <cobaltcyan.park@gmail.com>
2023-07-19 01:32:10 +0900

    [Comment] user API 계층구조 분리 주석 제거

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,19 +5,6 @@ const router = express.Router();
 const userTokenValidate = require("../middlewares/userTokenValidate");
 const isLoginValidate = require("../middlewares/isLoginValidate");
 
-// user API
-// const userApi = require("../apis/user");
-// router.get('/userApiTest', userApi.getAllUserInfo);                  // user API 테스트
-// router.get('/login', isLoginValidate);                              // user 로그인 유효성 검사 테스트
-// router.get('/userInfo/:user_id', userTokenValidate, userApi.getUserIdInfo);            // user_id 로 해당 사용자 정보 조회
-// router.post('/register', userApi.registerUser);
-// router.post('/login', userApi.loginUser); 
-// router.get('/mypage', userTokenValidate, userApi.getUserInfo);                         // token 값 header로 받아주기
-// router.put('/mypage', userTokenValidate, userApi.modifyUserInfo);
-// router.delete('/mypage', userTokenValidate, userApi.deleteUser);
-// router.post('/logout', userTokenValidate, userApi.logoutUser);
-
-
 // user API - Hierarchical Separation
 const userController = require("../controllers/userController");
 
