b243b80c11d520a39c56f66a0ae028795cdbc097
rhquddnr95 <39820865+rhquddnr95@users.noreply.github.com>
2023-08-03 00:49:51 +0900

    Feat : 모집 마감시 스터디 UI에 추가

--- src/components/study/StudyListItem.tsx
+++ src/components/study/StudyListItem.tsx
@@ -1,142 +1,166 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-import StyledIcon from "../layout/Img";
-import PeopleIconSrc from "../../img/people_navy.svg";
-import { colors } from "../../constants/colors";
-import * as fonts from "../../constants/fonts";
+import StyledIcon from '../layout/Img';
+import PeopleIconSrc from '../../img/people_navy.svg';
+import { colors } from '../../constants/colors';
+import * as fonts from '../../constants/fonts';
+import { getCurrentDate } from '../../utils/getCurrentDate';
+import { dateSplice } from '../../utils/dateFomatting';
 
 type StudyListProps = {
-  id: string;
-  title: string;
-  currentParticipants: number;
-  maxParticipants: number;
-  startDate: string;
-  endDate: string;
-  recruitDeadline: string;
-  master: string;
+    id: string;
+    title: string;
+    currentParticipants: number;
+    maxParticipants: number;
+    startDate: string;
+    endDate: string;
+    recruitDeadline: string;
+    master: string;
 };
 
+/** 스터디 간소 UI 컴포넌트 props:{
+    id,
+    title,
+    currentParticipants,
+    maxParticipants,
+    startDate,
+    endDate,
+    recruitDeadline,
+    master,
+}  */
 const StudyListItem: React.FC<StudyListProps> = ({
-  id,
-  title,
-  currentParticipants,
-  maxParticipants,
-  startDate,
-  endDate,
-  recruitDeadline,
-  master,
+    id,
+    title,
+    currentParticipants,
+    maxParticipants,
+    startDate,
+    endDate,
+    recruitDeadline,
+    master,
 }) => {
-  return (
-    <StyledStudyListContainer key={id}>
-      <StyledStudyListNavyArea>
-        <StyledStudyTagArea>
-          <StyledStudyRecruitTag>모집 중</StyledStudyRecruitTag>
-        </StyledStudyTagArea>
-        <StyledStudyName>{title}</StyledStudyName>
-      </StyledStudyListNavyArea>
+    const [recruitmentStatus, setRecruitmentStatus] = useState('모집 중');
+    const currentDate = getCurrentDate();
+    // 같은 형식으로 포멧팅
+    const formattedRecruitDeadline = `20${recruitDeadline}`.replaceAll('.', '-');
 
-      <StyledStudyListWhiteText>
-        <StyledStudyPeopleArea>
-          <StyledIcon src={PeopleIconSrc} />
-          <StyledStudyListPeopleText>
-            {currentParticipants} / {maxParticipants} 명
-          </StyledStudyListPeopleText>
-        </StyledStudyPeopleArea>
+    useEffect(() => {
+        if (formattedRecruitDeadline < currentDate) {
+            setRecruitmentStatus('모집 마감');
+        }
+    }, []);
 
-        <StyledStudyDateArea>
-          <StyledStudyListDateText>
-            진행 기간 | {startDate} ~ {endDate}
-          </StyledStudyListDateText>
-          <StyledStudyListDateText>
-            모집 마감 | {recruitDeadline}
-          </StyledStudyListDateText>
-        </StyledStudyDateArea>
+    const isRecruitmentClosed = recruitmentStatus === '모집 마감';
 
-        <StyledStudyMaster>{master}</StyledStudyMaster>
-      </StyledStudyListWhiteText>
-    </StyledStudyListContainer>
-  );
+    return (
+        <StyledStudyListContainer key={id}>
+            <StyledStudyListNavyArea>
+                <StyledStudyTagArea>
+                    <StyledStudyRecruitTag isClosed={isRecruitmentClosed}>{recruitmentStatus}</StyledStudyRecruitTag>
+                </StyledStudyTagArea>
+                <StyledStudyName>{title}</StyledStudyName>
+            </StyledStudyListNavyArea>
+
+            <StyledStudyListWhiteText>
+                <StyledStudyPeopleArea>
+                    <StyledIcon src={PeopleIconSrc} />
+                    <StyledStudyListPeopleText>
+                        {currentParticipants} / {maxParticipants} 명
+                    </StyledStudyListPeopleText>
+                </StyledStudyPeopleArea>
+
+                <StyledStudyDateArea>
+                    <StyledStudyListDateText>
+                        진행 기간 | {startDate} ~ {endDate}
+                    </StyledStudyListDateText>
+                    <StyledStudyListDateText>모집 마감 | {recruitDeadline}</StyledStudyListDateText>
+                </StyledStudyDateArea>
+
+                <StyledStudyMaster>{master}</StyledStudyMaster>
+            </StyledStudyListWhiteText>
+        </StyledStudyListContainer>
+    );
 };
 
 export default StudyListItem;
 
 const StyledStudyListContainer = styled.div`
-  width: 295px;
-  height: 295px;
-  border: solid 1px #dadada;
-  border-radius: 15px;
-  background-color: #fff;
-  margin: 0;
+    width: 295px;
+    height: 295px;
+    border: solid 1px #dadada;
+    border-radius: 15px;
+    background-color: #fff;
+    margin: 0;
 `;
 const StyledStudyListNavyArea = styled.div`
-  width: 295px;
-  height: 124px;
-  border: solid 1px #2e3057;
-  border-radius: 15px 15px 0 0;
-  background-color: #2e3057;
+    width: 295px;
+    height: 124px;
+    border: solid 1px #2e3057;
+    border-radius: 15px 15px 0 0;
+    background-color: #2e3057;
 `;
 const StyledStudyTagArea = styled.div`
-  width: fit-content;
-  height: 20px;
-  font-size: 12px;
-  margin: 35px 0 0 35px;
+    width: fit-content;
+    height: 20px;
+    font-size: 12px;
+    margin: 35px 0 0 35px;
 `;
-const StyledStudyRecruitTag = styled.div`
-  width: fit-content;
-  height: fit-content;
-  display: inline;
-  color: ${colors.main_mint};
-  border: solid 1px ${colors.main_mint};
-  border-radius: 15px;
-  padding: 1px 10px;
-  margin-right: 8px;
+/** 마감시 isClosed 가 true 이므로 색상 변경 */
+const StyledStudyRecruitTag = styled.div<{ isClosed: boolean }>`
+    width: fit-content;
+    height: fit-content;
+    display: inline;
+    color: ${(props) => (props.isClosed ? colors.main_red : colors.main_mint)};
+    border: solid 1px ${(props) => (props.isClosed ? colors.main_red : colors.main_mint)};
+    border-radius: 15px;
+    padding: 1px 10px;
+    margin-right: 8px;
 `;
 const StyledStudyName = styled.p`
-  width: 225px;
-  height: fit-content;
-  font-size: 24px;
-  font-weight: semibold;
-  color: #fff;
-  margin-left: 35px;
-  margin: 13px 0 0 35px;
-  overflow: hidden;
-  white-space: nowrap;
+    width: 225px;
+    height: fit-content;
+    font-size: 24px;
+    font-weight: semibold;
+    color: #fff;
+    margin-left: 35px;
+    margin: 13px 0 0 35px;
+    overflow: hidden;
+    white-space: nowrap;
 `;
 const StyledStudyListWhiteText = styled.div`
-  width: 225px;
-  height: fit-content;
-  margin: 20px 0 0 35px;
+    width: 225px;
+    height: fit-content;
+    margin: 20px 0 0 35px;
 `;
 const StyledStudyPeopleArea = styled.div`
-  display: flex;
-  font-size: 24px;
-  font-weight: semibold;
+    display: flex;
+    font-size: 24px;
+    font-weight: semibold;
 `;
 const StyledStudyDateArea = styled.div`
-  height: 44px;
-  font-size: 24px;
-  font-weight: semibold;
-  margin-top: 20px;
-  display: flex;
-  flex-wrap: wrap;
-  align-content: space-between;
+    height: 44px;
+    font-size: 24px;
+    font-weight: semibold;
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: space-between;
 `;
 const StyledStudyListPeopleText = styled.p`
-  font-size: 14px;
-  font-weight: medium;
-  color: black;
-  margin: 0 0 0 8px;
+    font-size: 14px;
+    font-weight: medium;
+    color: black;
+    margin: 0 0 0 8px;
 `;
 const StyledStudyListDateText = styled.p`
-  font-size: 14px;
-  font-weight: medium;
-  color: black;
-  margin: 0;
+    font-size: 14px;
+    font-weight: medium;
+    color: black;
+    margin: 0;
 `;
 const StyledStudyMaster = styled.p`
-  font-size: 14px;
-  font-weight: medium;
-  color: black;
-  margin: 20px 0 0 0;
+    font-size: 14px;
+    font-weight: medium;
+    color: black;
+    margin: 20px 0 0 0;
 `;
